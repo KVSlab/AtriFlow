4bc0e728493eaa0815775abf892e9de7bcba6d7a
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2024-09-23 16:11:50 +0200

    Add optimization for SR flow rates

---
+++ src/step_1_find_optimal_N.py
@@ -0,0 +1,255 @@
+import numpy as np
+import scipy
+from scipy.optimize import minimize
+
+from common import (
+    create_main_ref_sr,
+    get_pv_flow_rate,
+    get_cases,
+    load_data,
+)
+
+
+def compute_error(optimal_Q_avg, optimal_n, optimal_bpm):
+    # Calculate the values using the optimal parameters
+    co_calculated_opt, pv_calculated_opt, mv_calculated_opt = np.array(
+        [
+            calculate_co_mv_and_pv_peak(
+                optimal_Q_avg,
+                optimal_n,
+                optimal_bpm,
+                data,
+                model,
+                Q_ref,
+                area_avg,
+                volume_avg,
+            )
+            for data in all_model_data
+        ]
+    ).T
+    # Compute the errors
+    print(f"\n==== Computing errors for model: {model} ====")
+
+    relative_error_co = 100 * np.mean(
+        np.abs(co_calculated_opt - reference_co) / reference_co
+    )
+    relative_error_co_sd = (
+            100 * np.abs(np.std(co_calculated_opt) - reference_co_sd) / reference_co_sd
+    )
+
+    relative_error_mv = 100 * np.mean(
+        np.abs(mv_calculated_opt - reference_mv_velocity) / reference_mv_velocity
+    )
+    relative_error_mv_sd = (
+            100* np.abs(np.std(mv_calculated_opt) - reference_mv_velocity_sd) / reference_mv_velocity_sd
+    )
+
+    relative_error_pv = 100 * np.mean(
+        np.abs(pv_calculated_opt - reference_pv_velocity) / reference_pv_velocity
+    )
+    relative_error_pv_sd = (
+            100
+            * np.abs(np.std(pv_calculated_opt) - reference_pv_velocity_sd)
+            / reference_pv_velocity_sd
+    )
+
+    relative_error_bpm = 100 * np.abs(optimal_bpm - reference_bpm) / reference_bpm
+
+    # Print relative errors formatted with two decimal places
+    print(
+        "Error in CO (mean, SD): ({:.2f}%, {:.2f}%)".format(
+            relative_error_co, relative_error_co_sd
+        )
+    )
+    print(
+        "Error in MV (mean, SD): ({:.2f}%, {:.2f}%)".format(
+            relative_error_mv, relative_error_mv_sd
+        )
+    )
+    print(
+        "Error in PV (mean, SD): ({:.2f}%, {:.2f}%)".format(
+            relative_error_pv, relative_error_pv_sd
+        )
+    )
+    print("Error in BPM (mean): ({:.2f}%)".format(relative_error_bpm))
+
+    # Calculate the combined total relative error in percentage
+    total_relative_error = np.mean(
+        [relative_error_co, relative_error_pv, relative_error_mv, relative_error_bpm]
+    )
+    total_relative_error_sd = np.mean(
+        [relative_error_co_sd, relative_error_pv_sd, relative_error_mv_sd]
+    )
+    print("Average Mean error: {:.2f}%".format(total_relative_error))
+    print("Average SD error: {:.2f}%".format(total_relative_error_sd))
+
+    combined_error = np.mean([total_relative_error_sd, total_relative_error])
+    print("Total error: {:.2f}%".format(combined_error))
+
+
+def calculate_Q_A(Q_avg, n, area_i, Q_ref, area_avg):
+    Q_i = Q_avg * Q_ref * (area_i / area_avg) ** n
+    return Q_i
+
+
+def calculate_Q_V(Q_avg, n, volume_i, Q_ref, volume_avg):
+    Q_i = Q_avg * Q_ref * (volume_i / volume_avg) ** n
+    return Q_i
+
+
+def calculate_Q_umax(area_i, max_u):
+    Q_i = max_u * area_i
+    return Q_i
+
+
+def calculate_co_mv_and_pv_peak(
+        Q_avg, n, bpm, model_data, model, Q_ref, area_avg, volume_avg, max_u=None, max_pv=None
+):
+    t_max = 60 / bpm
+    scale_ms_to_cms = 100  # to cm/s
+    area_i = model_data["outlet_area"]
+    volume_i = np.min(model_data["volume"])
+
+    # Calculate Q_mv based on the formula
+    Q_mv = None
+    if model == "Q-A":
+        Q_mv = calculate_Q_A(Q_avg, n, area_i, Q_ref, area_avg)
+    elif model == "Q-V":
+        Q_mv = calculate_Q_V(Q_avg, n, volume_i, Q_ref, volume_avg)
+    elif model == "MaxMV":
+        Q_mv = calculate_Q_umax(area_i, max_u)
+    elif model == "MaxPV":
+        Q_mv = calculate_Q_umax(area_i, max_u)
+        # Calculate Q_pv based on volume
+        Q_pv, volume_splev = get_pv_flow_rate(t_max, Q_mv, model_data)
+        area_pv_total = sum([model_data[f"inlet{i}_area"] for i in range(4)])
+
+        Q_pv = Q_pv / np.max(Q_pv) * max_pv * area_pv_total
+        Q_wall = np.gradient(volume_splev, t_max / 1000)
+        Q_mv = Q_pv - Q_wall
+
+    if model != "MaxPV":
+        # Calculate Q_pv based on volume
+        Q_pv, _ = get_pv_flow_rate(t_max, Q_mv, model_data)
+
+    # Step 3: Compute Cardiac Output (CO)
+    dt = t_max / len(Q_mv)
+    sv = scipy.integrate.trapezoid(Q_mv, dx=dt)  # Average Q
+    co = (sv * bpm) / 1000  # Convert from ml to liters
+
+    # Compute MV and PV peak
+    area_pv_total = np.sum([model_data[f"inlet{i}_area"] for i in range(4)])  # mm^2
+    u_pv_peak = np.max(Q_pv) / area_pv_total * scale_ms_to_cms
+
+    u_mv_peak = np.max(Q_mv) / model_data["outlet_area"] * scale_ms_to_cms
+
+    return co, u_pv_peak, u_mv_peak
+
+
+def objective_function(params):
+    alpha, beta, bpm = params
+    w1 = w2 = w3 = w4 = 1.0
+    co_calculated, pv_calculated, mv_calculated = np.array(
+        [
+            calculate_co_mv_and_pv_peak(
+                alpha, beta, bpm, data, model, Q_ref, area_avg, volume_avg
+            )
+            for data in all_model_data
+        ]
+    ).T
+
+    # Standard deviations
+    co_calculated_sd = np.std(co_calculated)
+    pv_calculated_sd = np.std(pv_calculated)
+    mv_calculated_sd = np.std(mv_calculated)
+
+    # Calculate the components of your objective function element-wise
+    component1 = (
+            w1 * (np.abs(co_calculated - reference_co) / reference_co)
+            + w1 * np.abs(co_calculated_sd - reference_co_sd) / reference_co_sd
+    )
+    component2 = (
+            w2 * (np.abs(pv_calculated - reference_pv_velocity) / reference_pv_velocity)
+            + w2 * np.abs(pv_calculated_sd - reference_pv_velocity_sd) / reference_pv_velocity_sd
+    )
+    component3 = (
+            w3 * (np.abs(mv_calculated - reference_mv_velocity) / reference_mv_velocity)
+            + w3 * np.abs(mv_calculated_sd - reference_mv_velocity_sd) / reference_mv_velocity_sd
+    )
+    component4 = w4 * np.abs(bpm - reference_bpm) / reference_bpm
+
+    # Calculate the total objective function as the sum of components
+    objective_function = (
+            np.sum(component1)
+            + np.sum(component2)
+            + np.sum(component3)
+            + np.sum(component4)
+    )
+
+    return objective_function
+
+
+def perform_optimization(model):
+    # Initial guess for alpha, beta, and BPM
+    exponent_guess = 0  # n, [-]
+    bpm_guess = reference_bpm  # Heart rate, [BPM]
+    Q_avg_guess = 74.75  # Ref CO / Ref BPM * 1000
+    initial_guess = np.array([Q_avg_guess, exponent_guess, bpm_guess])
+
+    # Model for flow rate
+    optimal_values = []
+
+    # Run the optimization
+    min_Q_avg = 50
+    max_Q_avg = 150
+
+    min_n = 0
+    max_n = 3
+
+    # Based on mean +/- SD
+    min_bpm = 40
+    max_bpm = 100
+
+    method = "L-BFGS-B"
+    result = minimize(
+        objective_function,
+        initial_guess,
+        method=method,
+        bounds=[(min_Q_avg, max_Q_avg), (min_n, max_n), (min_bpm, max_bpm)],
+    )
+
+    results = result.x  # The optimal values
+    optimal_values.append(results)
+    optimal_Q_avg = optimal_values[0][0]
+    optimal_n = optimal_values[0][1]
+    optimal_bpm = optimal_values[0][2]
+
+    print(
+        f"\nOptimal values for model {model} with method {method}: "
+        + f"\nQ_avg={optimal_Q_avg:.2f} "
+        + f"\nn={optimal_n:.2f} "
+        + f"\nHR_SR={optimal_bpm:.2f}"
+    )
+    # Calculate errors:
+    compute_error(optimal_Q_avg, optimal_n, optimal_bpm)
+
+
+if __name__ == "__main__":
+    # Get references
+    reference_dict = create_main_ref_sr()
+    reference_co = reference_dict["co"]
+    reference_co_sd = reference_dict["co_sd"]
+    reference_pv_velocity = reference_dict["u_pv"]
+    reference_pv_velocity_sd = reference_dict["u_pv_sd"]
+    reference_mv_velocity = reference_dict["u_mv"]
+    reference_mv_velocity_sd = reference_dict["u_mv_sd"]
+    reference_bpm = reference_dict["hr"]
+    reference_bpm_sd = reference_dict["hr_sd"]
+
+    # Set condition and load input data
+    condition = "sr"
+    cases = get_cases()
+    Q_ref, area_avg, volume_avg, all_model_data = load_data(cases, condition)
+
+    for model in ["Q-A", "Q-V"]:
+        perform_optimization(model)
---
+++ src/step_2_compute_sr_flow_rates.py
@@ -0,0 +1,117 @@
+from os import mkdir, path
+
+import numpy as np
+import scipy
+from scipy.interpolate import splev, splrep
+
+from common import (
+    get_mv_flow_rate_for_condition,
+    get_optimal_values,
+    get_pv_flow_rate,
+    get_cases,
+    load_data,
+    plot_flow_rate,
+    SAVE_PATH,
+)
+
+
+def create_plot_and_save_sr_flow_rate(cases, datas, Q_bosi, model, condition):
+    t_end = 60 / optimal_bpm
+    t_max = int(t_end * 1000)
+    time = np.linspace(0, t_max / 1000, 1000)
+    dt = t_max / 1000 / len(time)
+
+    # Scale to cm/s
+    velocity_scale = 100
+
+    co_values = []
+    u_pvs_peak = []
+    u_mvs_peak = []
+    q_pvs = []
+    q_mvs = []
+    u_pvs = []
+    volumes = []
+
+    for i, case in enumerate(cases):
+        info = datas[i]
+        area_mv = info["outlet_area"]
+        volume_la = np.min(info["volume"])
+
+        # Get MV flow rate
+        Q_mv = get_mv_flow_rate_for_condition(
+            time, area_mv, area_avg, volume_la, volume_avg, Q_bosi, optimal_n, model, condition
+        )
+
+        # Add Q_wall to compute Q_pv
+        Q_pv, volume_splev = get_pv_flow_rate(t_end, Q_mv, info)
+
+        # Compute max Q_PV, max Q_MV, and CO
+        compute_mean_and_sd(
+            dt, co_values, Q_mv, info, velocity_scale, u_mvs_peak, u_pvs_peak, Q_pv
+        )
+
+        s_v = 1e-2
+        Q_splrep = splrep(time, Q_pv, s=s_v, per=True)
+        Q_splev = splev(time, Q_splrep)
+
+        # Plot Velocity
+        area_pv_total = sum([info[f"inlet{i}_area"] for i in range(4)])
+        u_pv = Q_splev / area_pv_total * velocity_scale
+        Q_pv = Q_splev
+
+        save_path = SAVE_PATH.format(condition)
+        if not path.isdir(save_path):
+            mkdir(save_path)
+        np.savetxt(
+            path.join(save_path, f"flow_rate_{case}_{model}.txt"),
+            np.array([time, Q_pv]).T,
+        )
+
+        # Plot curves
+        volumes.append(volume_splev - volume_splev[0])
+        u_pvs.append(u_pv)
+        q_pvs.append(Q_pv)
+        q_mvs.append(Q_mv)
+
+    plot_flow_rate(cases, u_pvs, time)
+    print()
+    print(f"Mean and SD values for model: {model}")
+    print("CO", np.mean(co_values), np.std(co_values))
+    print("U_PV", np.mean(u_pvs_peak), np.std(u_pvs_peak))
+    print("U_MV", np.mean(u_mvs_peak), np.std(u_mvs_peak))
+    print()
+
+
+def compute_mean_and_sd(dt, co_values, Q_splev, info, scale, u_mvs, u_pvs, Q_pv):
+    sv = scipy.integrate.trapezoid(Q_splev, dx=dt)  # Stroke volume
+    co = (optimal_bpm * sv) / 1000
+    co_values.append(co)
+
+    area_pv_total = sum([info[f"inlet{i}_area"] for i in range(4)])
+    Q_mv_max = np.max(Q_splev)
+    Q_pv_max = np.max(Q_pv)
+    u_mv_peak = Q_mv_max / info["outlet_area"] * scale
+    u_pv_peak = Q_pv_max / area_pv_total * scale
+    u_pvs.append(u_pv_peak)
+    u_mvs.append(u_mv_peak)
+
+
+if __name__ == "__main__":
+    cases = get_cases()
+    Q_ref, area_avg, volume_avg, all_model_data = load_data(cases, "sr")
+
+    # From step_1.py
+    Q_avgs, n_values, bpms, _, _ = get_optimal_values()
+    condition = "sr"
+    print(f"-- Storing flow rates in {SAVE_PATH.format(condition)}")
+
+    models = ["Q-A", "Q-V"]
+    for model in models:
+        optimal_Q_avg = Q_avgs[model]
+        optimal_n = n_values[model]
+        optimal_bpm = bpms[model]
+
+        Q_bosi = optimal_Q_avg * Q_ref
+
+        # Create flow rate
+        create_plot_and_save_sr_flow_rate(cases, all_model_data, Q_bosi, model, condition)
